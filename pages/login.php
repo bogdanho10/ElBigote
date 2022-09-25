c9f81f2eddfc9f0da3f7bc193bf0def8ebd1b883
bogdanho10 <62665199+bogdanho10@users.noreply.github.com>
2022-09-25 21:06:13 +0100

    Add files via upload

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,6 @@
 <h1>Login Page</h1>
-<form action="" method="post">
-    Username: <input type="text" name="name"><br>
-    Password: <input type="text" name="password"><br>
-    <input type="submit" name="loginButton">
+<form class="loginForm" action="" method="post">
+    <input type="text" name="name" placeholder="Username"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="submit" name="loginButton" value="Log in">
 </form>
--- pages/register.php
+++ pages/register.php
@@ -1,11 +1,11 @@
 <h1>Register Page</h1>
-<form action="index.php?page=register.inc" method="post">
-    <input type="text" name="firstName" placeholder="First Name">
-    <input type="text" name="lastName" placeholder="Last Name">
-    <input type="text" name="gender" placeholder="Gender">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <input type="password" name="passwordRepeat" placeholder="Repeat Password">
-    <input type="text" name="phoneNumber" placeholder="Phone Number">
+<form class="registerForm" action="index.php?page=register.inc" method="post">
+    <input type="text" name="firstName" placeholder="First Name"><br>
+    <input type="text" name="lastName" placeholder="Last Name"><br>
+    <input type="text" name="gender" placeholder="Gender"><br>
+    <input type="email" name="email" placeholder="Email"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="password" name="passwordRepeat" placeholder="Repeat Password"><br>
+    <input type="text" name="phoneNumber" placeholder="Phone Number"><br>
     <button type="submit" name="registerButton">Register</button>
 </form>
